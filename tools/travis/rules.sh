931b0d3099bdc4b5ad125d47ea8deef14e57794b
Erlend Klakegg Bergheim <erlend@klakegg.net>
2017-12-31 10:42:11 +0100

    Updating Travis scripts.

--- tools/travis/rules.sh
+++ tools/travis/rules.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# Generate basic rules for Schematron
+
+docker run --rm -it \
+-v $(pwd):/src \
+-v $(pwd)/target/generated:/target \
+klakegg/saxon \
+-s:structure/espd-request/structure.xml \
+-xsl:tools/xslt/structure-schematron.xslt \
+-o:/target/EHF-ESPD-REQUEST-BASIC.sch \
+pattern_only=true
+
+docker run --rm -it \
+-v $(pwd):/src \
+-v $(pwd)/target/generated:/target \
+klakegg/saxon \
+-s:structure/espd-response/structure.xml \
+-xsl:tools/xslt/structure-schematron.xslt \
+-o:/target/EHF-ESPD-RESPONSE-BASIC.sch \
+pattern_only=true
+
 # Validator build
 
 docker run --rm -it -v $(pwd):/src difi/vefa-validator build -x -t /src -t -target target/validator
