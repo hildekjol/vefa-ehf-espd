7d73f52331fb95a4c356e75cffda6eb4d4dac6b4
Erlend Klakegg Bergheim <erlend@klakegg.net>
2018-01-06 22:33:34 +0100

    Using docker-compose for build.

--- tools/travis/guides.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-FOLDER=$(dirname $(readlink -f "$0"))/../..
-. $FOLDER/tools/script/common.sh
-
-info "Generate documentation artifacts"
-docker run --rm -i -v $FOLDER:/documents -v $FOLDER/target:/target difi/asciidoctor
